6d833122487ad3d6c48f6dba851f7c19835df523
Furkan Arslan <furkanarslandev@gmail.com>
2025-06-30 14:17:17 +0300

    feat:register_route

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,48 @@
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { firstName, lastName, username, email, password } = body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    if (!email || !password || !username || !firstName || !lastName) {
+      return NextResponse.json(
+        { message: "all fields are required" },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    const user = await prisma.user.create({
+      data: { firstName, lastName, username, email, password: hashedPassword },
+    });
+
+    const { password: _, ...userData } = user;
+
+    return NextResponse.json(
+      { message: "User created succesfully", user: userData },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.error("Register error", err);
+
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
