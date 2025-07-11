bf92c08e5e9d0f9e8cc301ed70c6f8a7e44816fc
Furkan Arslan <furkanarslandev@gmail.com>
2025-07-11 15:06:17 +0300

    feat:add user_profile_avatarImage

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useauthStore } from "@/lib/stores/authStore";
+import Image from "next/image";
 
 export default function ProfilePage() {
   const user = useauthStore((state) => state.user);
@@ -8,9 +9,23 @@ export default function ProfilePage() {
   if (!user) return <p>Loading...</p>;
 
   return (
-    <div>
-      <h1 className="text-xl font-bold">Profil: {user.firstName}</h1>
-      <p>Email: {user.email}</p>
+    <div className="space-y-6 border p-6 rounded-2xl w-fit lg:w-4xl mx-auto text-center">
+      <h1 className="text-xl font-bold border-b-2 pb-2">User Profile</h1>
+      {user.avatarUrl && (
+        <div className="relative aspect-square w-40 mx-auto">
+          <Image
+            src={user.avatarUrl}
+            alt={user.username}
+            fill
+            className="object-cover rounded-full"
+          />
+        </div>
+      )}
+      <div className="flex flex-col gap-6">
+        <p className="text-xl font-bold">First name: {user.firstName}</p>
+        <p className="text-xl font-bold">Last name: {user.lastName}</p>
+        <p>Email: {user.email}</p>
+      </div>
     </div>
   );
 }
