f7eae789c6d405bd332e5e7273a65358ed407cc3
Furkan Arslan <furkanarslandev@gmail.com>
2025-07-12 21:55:02 +0300

    fix:eslint

--- src/components/pagination/pagination-controls.tsx
+++ src/components/pagination/pagination-controls.tsx
@@ -5,11 +5,10 @@ import Link from "next/link";
 export function PaginationControls({
   currentPage,
   totalPages,
-   categoryId,
 }: {
   currentPage: number;
   totalPages: number;
-    categoryId?: string;
+  categoryId:string
 }) {
   // create a array for contain page numbers
   const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
